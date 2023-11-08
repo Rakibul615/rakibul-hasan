b2ca1865ddaccc27a5051324617eba7bcb01ff56
Rakibul <rakibul01072019@gmail.com>
2023-11-08 13:29:10 +0600

    done

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -21,7 +21,7 @@
                     <a class="nav-link active" aria-current="page" href="{{route('index')}}">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Manage Task</a>
+                    <a class="nav-link" href="{{route('create')}}">Create</a>
                 </li>
 
 
--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -1,48 +1,27 @@
 @extends('master')
 @section('content')
-    <section class="py-5 bg-info">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-7 mx-auto">
-                    <div class="card card-body">
-                        <form action="{{route('store')}}" method="POST">
-                            @csrf
-                            <div class="row mb-3">
-                                <label for="" class="col-md-3"> Title</label>
-                                <div class="col-md-9">
-                                    <input type="text" class="form-control" name="title">
-                                </div>
-                            </div>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col">Completed</th>
+            <th scope="col">Action</th>
 
-                            <div class="row mb-3">
-                                <label for="" class="col-md-3"> Description</label>
-                                <div class="col-md-9">
-                                    <textarea type="text" class="form-control" name="description"></textarea>
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <label for="" class="col-md-3"> Completed</label>
-                                <div class="col-md-9">
-                                    <input type="hidden" name="bool" value="0">
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>
 
-                                </div>
-                            </div>
+            </td>
+        </tr>
 
-                            <div class="row mb-3">
-                                <label for="" class="col-md-3"></label>
-                                <div class="col-md-9">
-                                    <input type="submit" class="btn btn-success" value="Submit">
-                                </div>
-                            </div>
-
-
-
-                        </form>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </section>
+        </tbody>
+    </table>
 @endsection
